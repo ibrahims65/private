23fc0ef9bf83c3ec2fe9f03fd207106db82fef9d
ibrahims65 <ibrahims65@gmail.com>
2024-07-30 08:05:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -88,4 +88,15 @@ function displayResults(results) {
 // Collapsible logic
 document.addEventListener('DOMContentLoaded', () => {
     const coll = document.getElementsByClassName("collapsible");
-    for (let i = 0; i < coll.length;
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+});
